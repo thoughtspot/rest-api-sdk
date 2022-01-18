ee09d6376869de80393e243af7ab584ae05e7b97
Bandapelli Raj. Kumar <bandapelli.kumar@thoughtspot.com>
2022-01-18 19:03:04 +0530

    SCAL-105215 Java SDK regression testing framework

---
+++ java-sdk-test/src/main/java/com/jcg/maven/App.java
@@ -0,0 +1,14 @@
+package com.jcg.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
