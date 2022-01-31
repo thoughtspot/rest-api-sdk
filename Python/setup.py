d8df1f7be1cc76c30e0c9317fec6e6173bb67869
amenonts <88720212+amenonts@users.noreply.github.com>
2022-01-31 12:22:08 +0530

    Update setup.py

--- Python/setup.py
+++ Python/setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='thoughtspot-rest-api-sdk',
-    version='1.6.7',
+    version='1.8.0-alpha',
     description='Python client library for RESTAPI SDK',
     long_description=long_description,
     long_description_content_type="text/markdown",
