18345852496c943e74b44b419e1d715dd5586de1
Akash Mohapatra <akash.mohapatra@thoughtspot.com>
2022-07-13 19:11:26 +0530

    Style updates

--- api-playground/build.sh
+++ api-playground/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# cleanup generated portal and build archives
-rm -rf build
-mkdir build
+# cleanup generated portal, build archives and spec
+rm -rf build spec
+mkdir build spec
 
+# prepare spec
+cp ../api-spec/* spec/
+
+# native zip and unzip unavailable
 npx bestzip $PWD/build/portal-input.zip .
 
 curl -X POST --url 'https://www.apimatic.io/api/portal' -H "Authorization:X-Auth-Key "$APIMATIC_API_KEY"" -F 'file=@./build/portal-input.zip' -o ./build/portal-output.zip
