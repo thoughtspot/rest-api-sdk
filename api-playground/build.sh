5a732125d16f4041db33b69ce947ba8d3b4497ca
Ashish Shubham <ashubham@gmail.com>
2022-10-07 15:01:01 -0700

    update build script

--- api-playground/build.sh
+++ api-playground/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # cleanup generated portal, build archives and spec
-rm -rf build spec
-mkdir build spec
+rm -rf $PWD/build $PWD/spec
+mkdir $PWD/build $PWD/spec
 
 # prepare spec
 cp ../api-spec/* spec/
@@ -13,3 +13,4 @@ npx bestzip $PWD/build/portal-input.zip .
 curl -X POST --url 'https://www.apimatic.io/api/portal' -H "Authorization:X-Auth-Key "$APIMATIC_API_KEY"" -F 'file=@./build/portal-input.zip' -o ./build/portal-output.zip
 
 npx extract-zip $PWD/build/portal-output.zip $PWD/build/static-portal
+ls -R $PWD/build/static-portal
