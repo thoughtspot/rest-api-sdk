ae7880aa8d6df526739cf439d174bc2654596aee
abdullasulaiman <abdullasulaiman@gmail.com>
2023-03-13 19:44:01 +0530

    Update embedded.js

--- api-playground/static/js/embedded.js
+++ api-playground/static/js/embedded.js
@@ -9,20 +9,20 @@ let isApiMaticPortalReady = false;
 
 const patchURLAndPlayground = async ({ baseUrl, accessToken }) => {
   // find the configure button element
-  const configButtonElement = await getElementByIdAsync('code-config-button');
+  const configButtonElement = await getElementByIdAsync("code-config-button");
   if (!configButtonElement) return;
   configButtonElement.click(); // programatically click on configure button
   // Get all Form elements to control flickering the configuration form
-  const formElements = document.querySelectorAll('form');
+  const formElements = document.querySelectorAll("form");
   const formElement = formElements[formElements.length - 1]?.parentElement;
   if (!formElement) return;
-  formElement.style.display = 'none';
-  configButtonElement.style.display = 'block';
+  formElement.style.display = "none";
+  configButtonElement.style.display = "block";
   setTimeout(() => {
     // find the input element for to update base_url
     const inputElement = document.querySelector('input[label="base-url"]');
     if (!inputElement) return;
-    const event = new Event('input', { bubbles: true }); // create an input event to update base_url
+    const event = new Event("input", { bubbles: true }); // create an input event to update base_url
     const previousValue = inputElement.value;
     inputElement.value = baseUrl; // setup host and port url to input elment
     // eslint-disable-next-line no-underscore-dangle
@@ -33,7 +33,7 @@ const patchURLAndPlayground = async ({ baseUrl, accessToken }) => {
       'input[label="AccessToken"]'
     );
     if (!accesTokenInputElement) return;
-    const accessTokenEvent = new Event('input', { bubbles: true }); // create an input event to update accesstoken
+    const accessTokenEvent = new Event("input", { bubbles: true }); // create an input event to update accesstoken
     const accessTokenPreviousValue = accesTokenInputElement.value;
     accesTokenInputElement.value = accessToken; // set it up access token get from api
     // eslint-disable-next-line no-underscore-dangle
@@ -41,7 +41,7 @@ const patchURLAndPlayground = async ({ baseUrl, accessToken }) => {
     accesTokenInputElement.dispatchEvent(accessTokenEvent); // dispatch access token input elment
     accesTokenInputElement.focus();
     // to hide configure form finding titleElement and clicking programatically
-    const titleElement = document.getElementsByClassName('sc-kEjbQP');
+    const titleElement = document.getElementsByClassName("sc-kEjbQP");
     if (!titleElement || titleElement.length === 0) return;
     titleElement[0].click();
     shouldPatch = false;
@@ -51,7 +51,7 @@ const patchURLAndPlayground = async ({ baseUrl, accessToken }) => {
 const channel = new MessageChannel();
 let playgroundConfig = null;
 
-window.parent.postMessage({ type: 'api-playground-ready' }, '*', [
+window.parent.postMessage({ type: "api-playground-ready" }, "*", [
   channel.port2,
 ]);
 
@@ -70,34 +70,40 @@ function getElementByIdAsync(id) {
   });
 }
 
-document.getElementsByClassName('portal-header')[0].style.display = 'none';
+document.getElementsByClassName("portal-header")[0].style.display = "none";
 
 const setAPIMaticPortalConfig = () => {
-  APIMaticDevPortal.ready(({ setConfig })=> {
-      isApiMaticPortalReady = true;
-      _setConfig = setConfig;
-      // To run this first time
-      if(playgroundConfig) {
-        setPlaygroundConfig(playgroundConfig);
-      }
+  APIMaticDevPortal.ready(({ setConfig }) => {
+    isApiMaticPortalReady = true;
+    setConfig((defaultConfig) => {
+      return {
+        ...defaultConfig,
+        showFullCode: false,
+        config: {
+          ...defaultConfig.config,
+          "Accept-Language": "Typescript",
+          AccessToken: playgroundConfig.access,
+          "base-url": playgroundConfig.baseUrl,
+        },
+      };
+    });
   });
-}
+};
 
 /** setting APIMatic Portal */
 setAPIMaticPortalConfig();
 
-window.addEventListener('hashchange', (e) => {
+window.addEventListener("hashchange", (e) => {
   if (!shouldPatch) {
     return;
   }
   setPlaygroundConfig(playgroundConfig);
 });
 
-window.addEventListener('message', (event) => {
-  if (event.data?.type === 'api-playground-config') {
+window.addEventListener("message", (event) => {
+  if (event.data?.type === "api-playground-config") {
     shouldPatch = true;
     playgroundConfig = event.data;
-    if(isApiMaticPortalReady) { setPlaygroundConfig(playgroundConfig); }
     if (playgroundConfig.apiResourceId) {
       navigateEndpoint(playgroundConfig.apiResourceId);
     }
@@ -105,18 +111,18 @@ window.addEventListener('message', (event) => {
 });
 
 const setPlaygroundConfig = ({ baseUrl, access }) => {
-    _setConfig((defaultConfig) => {
-      return {
-        ...defaultConfig,
-        showFullCode: false,
-        config: {
-          ...defaultConfig.config,
-          AccessToken: access,
-          'base-url': baseUrl,
-        }
-      };
-    });
-}
+  _setConfig((defaultConfig) => {
+    return {
+      ...defaultConfig,
+      showFullCode: false,
+      config: {
+        ...defaultConfig.config,
+        AccessToken: access,
+        "base-url": baseUrl,
+      },
+    };
+  });
+};
 
 window.test = (config) => {
   setPlaygroundConfig(playgroundConfig);
