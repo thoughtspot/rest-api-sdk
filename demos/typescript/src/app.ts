13522ea69b57755e346cd95d35f1341dcd8cfc63
sastaachar <justinjohnmathew100@gmail.com>
2023-06-21 19:30:12 +0530

    Cleaned up folders

---
+++ demos/typescript/src/app.ts
@@ -0,0 +1,76 @@
+import {
+  AuthenticationApi,
+  GetFullAccessTokenRequest,
+  ServerConfiguration,
+  createConfiguration,
+  BaseServerConfiguration,
+  AuthMethodsConfiguration,
+  ThoughtSpotRestApi,
+} from "thoughtspot-rest-api-sdk";
+
+const BASE_URL = "<CLUSTER-URL>";
+
+const createBearerAuthenticationConfig = (
+  url,
+  params: GetFullAccessTokenRequest
+) => {
+  const serverConfig: BaseServerConfiguration = new ServerConfiguration<{}>(
+    url,
+    {}
+  );
+
+  const config = createConfiguration({
+    baseServer: serverConfig,
+  });
+  const authApiClient = new AuthenticationApi(config);
+
+  const authConfig: AuthMethodsConfiguration = {
+    bearerAuth: {
+      tokenProvider: {
+        getToken: async () => {
+          console.log("Calling Auth api to get token...");
+          const token = (await authApiClient.getFullAccessToken(params)).token;
+
+          console.log("Received token : ", token);
+
+          return token;
+        },
+      },
+    },
+  };
+
+  const globalConfig = createConfiguration({
+    authMethods: authConfig,
+    baseServer: serverConfig,
+  });
+
+  return globalConfig;
+};
+
+const main = async () => {
+  // getting the token
+
+  const config = createBearerAuthenticationConfig(BASE_URL, {
+    username: "tsadmin",
+    password: "admin",
+  });
+
+  const tsRestApiClient = new ThoughtSpotRestApi(config);
+
+  try {
+    const users = await tsRestApiClient.searchUsers();
+    console.log("Users on the cluster : ");
+    users.forEach((user) => {
+      console.log(user.name, user.email);
+    });
+    console.log("UserGroups on the cluster : ");
+    const groups = await tsRestApiClient.searchUserGroups();
+    groups.forEach((group) => {
+      console.log(group.name, group.displayName);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+main();
